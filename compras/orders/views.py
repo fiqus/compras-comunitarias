bb24bd4efcfa573d6442cc155a7e223586d1ee06
Mariano Lambir <mlambir@gmail.com>
2021-03-16 19:54:34 -0300

    update current order

--- compras/orders/views.py
+++ compras/orders/views.py
@@ -1,4 +1,5 @@
 import itertools
+from collections import defaultdict
 from typing import Any, Dict
 
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -27,21 +28,28 @@ def create_order(request):
         listing = Listing.objects.get(enabled=True)
     except Listing.DoesNotExist:
         return render(request, 'orders/no_listing.html')
+
+    order = Order.objects.filter(user=request.user, listing=listing).last()
     OrderProductInlineFormset = inlineformset_factory(Order, OrderProduct, fields=('product', 'amount'),
                                                       can_delete=False, extra=listing.listingproduct_set.count())
     if request.method == "POST":
-        form = OrderForm(request.POST, request.FILES, initial={'listing': listing})
+        form = OrderForm(request.POST, request.FILES, initial={'listing': listing}, instance=order)
         formset = OrderProductInlineFormset(request.POST, request.FILES, instance=form.instance)
-        if form.is_valid and formset.is_valid():
+        if form.is_valid() and formset.is_valid():
             form.instance.user = request.user
             form.instance.listing = listing
+            form.instance.orderproduct_set.all().delete()
             form.save()
             formset.save()
-            return render(request, 'orders/order_success.html', {'order': form.instance})
+            order = Order.objects.get(pk=form.instance.pk)
+            return render(request, 'orders/order_success.html', {'order': order})
     else:
         form = OrderForm()
-        formset = OrderProductInlineFormset(instance=form.instance,
-                                            initial=[{'product': lp.id, 'amount': 0} for lp in
-                                                     listing.listingproduct_set.all()])
+        formset = OrderProductInlineFormset(instance=form.instance)
+    amounts = defaultdict(int)
+    if order:
+        for p in order.orderproduct_set.all():
+            amounts[p.product.id] = p.amount
     return render(request, 'orders/order_form.html', {'form': form, 'formset': formset, 'listing': listing,
+                                                      'amounts': amounts, 'order': order,
                                                       'iterator': TemplateCounter()})
