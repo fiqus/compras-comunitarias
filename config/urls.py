ec87edf0bb6277acdca4bfa927ecf809ebb60462
MJoaaquin <joaquinmansilla@fiqus.com>
2022-01-28 11:12:15 -0300

    adding api app

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
     path("orders/", include("compras.orders.urls", namespace="orders")),
+    path("api/", include("api.urls", namespace="api")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
