5276a0aa84e554fa573038da0cc6d5897796cddc
JeronimoC23 <jeronimoclinaz@gmail.com>
2022-02-03 16:21:38 -0300

    add channel layer comunication with the view

--- api/views.py
+++ api/views.py
@@ -9,6 +9,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
 
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
 # Create your views here.
 def index(request):
     return render(request, 'api_test/index.html', {})
@@ -27,8 +33,18 @@ class OrderStatus(APIView):
         order = Order.objects.get(pk=data["order_id"])
         order.status = data["status"]
         order.save()
+
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send(
+            'compras-admin',
+            {
+                'change': '1'
+            }
+        ))
+        
         return Response()
 
 
 def consult(request):
     return render(request, 'api_test/consult.html', {})
+
