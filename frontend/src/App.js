32db32d4fb555fb7a487c65469b1716e2a5a5e61
MJoaaquin <joaquinmansilla@fiqus.com>
2022-02-10 14:28:21 -0300

    moving logic of get token and get orders to realtime page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,10 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 
-import { useRecoilState } from 'recoil';
-import { userTokensState, ordersState, listingIdState } from './state';
-import { httpGet } from './apiClient';
-
-import { Routes, Route, useSearchParams } from "react-router-dom";
-import WebSocketInstance from './socket';
+import { Routes, Route } from "react-router-dom";
 import RealTimePage from './pages/RealTimePage';
 import Login from './pages/Login';
 
 function App() {
-  const [queryParamas, _setQueryParams] = useSearchParams()
-  const [listingId, setListingId] = useRecoilState(listingIdState)
-
-  useEffect(() => {
-      setListingId(queryParamas.get("listingId"))
-  }, [queryParamas])
-
-  const [userTokens, _] = useRecoilState(userTokensState);
-  const [_orders, setOrders] = useRecoilState(ordersState);
-
-  const buildHeaders = () => {
-    return {"Authorization": `Token ${userTokens.token}`}
-  }
-
-  const getOrders = async () => {
-    const res = await httpGet(`/listing/${listingId}/orders`, {}, buildHeaders());
-    setOrders(res.data);
-  }
-
-  const refresher = () => getOrders();
-
-  useEffect(() => {
-    WebSocketInstance.addCallbacks(refresher.bind());
-    WebSocketInstance.connect();
-    
-    getOrders();
-  }, [userTokens, listingId])
-
-
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
--- frontend/src/apiClient.js
+++ frontend/src/apiClient.js
@@ -2,26 +2,39 @@ const apiURL = "http://localhost:8000/api";
 
 const axios = require("axios").create({baseURL: apiURL, timeout: 0, headers: {}});
 
+const buildHeaders = ({token}) => {
+    if (token) {
+        return {"Authorization": `Token ${token}`}
+    } else {
+        return {}
+    };
+};
+
 /* HTTP METHODS */
 
 export function httpGet(url, params = {}, headers={}) {
-    return axios.get(url, {headers, params});
-}
+    const constructedHeaders = buildHeaders(headers)
+    return axios.get(url, {builderedHeaders: constructedHeaders, params});
+};
 
 export function httpPost(url, data, headers={}) {
-    return axios.post(url, data, {headers});
-}
+    const constructedHeaders = buildHeaders(headers)
+    return axios.post(url, data, {constructedHeaders});
+};
 
 export function httpPut(url, data, headers={}) {
-    return axios.put(url, data, {headers});
-}
+    const constructedHeaders = buildHeaders(headers)
+    return axios.put(url, data, {constructedHeaders});
+};
 
 export function httpPatch(url, data, headers={}) {
-    return axios.patch(url, data, {headers});
-}
+    const constructedHeaders = buildHeaders(headers)
+    return axios.patch(url, data, {constructedHeaders});
+};
 
 export function httpDelete(url, headers={}) {
-    return axios.delete(url, {headers});
-}
+    const constructedHeaders = buildHeaders(headers)
+    return axios.delete(url, {constructedHeaders});
+};
 
 /* END HTTP METHODS */
