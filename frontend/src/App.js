b7547f5c6974d42d5192071365e18b5014e3075d
MJoaaquin <joaquinmansilla@fiqus.com>
2022-02-10 15:35:52 -0300

    working on routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import RealTimePage from './pages/RealTimePage';
 import Login from './pages/Login';
+import { useRecoilState } from 'recoil';
+import { userTokensState } from './state';
 
 function App() {
+  const [userToken, _setUserToken] = useRecoilState(userTokensState)
+
+  const whereNavigate = () => {
+    if (!userToken) {
+      return <Navigate to="/login" /> 
+    } else {
+      // TODO: acá deberiamos tener logica que sepa dependiendo
+      // que rol tenga (ADMIN o CONSUMIDOR) donde tiene que ir
+      return <Navigate to="/realtime" /> 
+    }
+  }
+
   return (
     <Routes>
+      <Route path="/" element={whereNavigate()} />
       <Route path="/login" element={<Login />} />
       <Route path="/realtime" element={<RealTimePage />} />
     </Routes>
