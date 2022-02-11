163a3db300e791608af44a96d8b2638dbcd8c3c8
ezequielleiter <ezequielleiter@gmail.com>
2022-02-11 20:50:15 -0300

    SigIn Page

---
+++ frontend/user-frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SignIn from './pages/SigIn';
+
+function App() {
+  return (
+    <>
+  <SignIn/>
+    </>
+  );
+}
+
+export default App;
