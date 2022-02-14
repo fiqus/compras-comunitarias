8e8233ff943cf8260c0aa25b8fbab005e73f03be
ezequielleiter <ezequielleiter@gmail.com>
2022-02-14 15:36:16 -0300

    maquetado de compras activas/proximas, falta nav

--- frontend/frontendoffice/src/pages/ComprasActivas.js
+++ frontend/frontendoffice/src/pages/ComprasActivas.js
@@ -25,11 +25,15 @@ const SigInButton = styled(Button)(({ theme }) => ({
 export default function ComprasActivas() {
   return (
   <>
+  
   <NavBar />
   <Container fixed sx={{marginTop: 2}}>
+{/* Compras activas */}
+  <Grid container sx={{displey: "flex", justifyContent: {xs: "center", md: 'left'}}}>
   <Typography sx={{fontSize: 20, fontWeight: "bold"}}>Compras activas</Typography>
+  </Grid>
   <Divider component="div" />
-  
+
   {/* Tarjeta de compras */}
       <Grid container  wrap="nowrap" spacing={0} sx={{marginTop: 2, border: "1px solid black", marginLeft: 0, marginRight: 0,}}>
         <Grid>
@@ -51,8 +55,6 @@ export default function ComprasActivas() {
         </Grid>
       </Grid>
       <SigInButton fullWidth variant="contained" sx={{marginTop: 1}}>Comprar</SigInButton>
-    
-
       <Grid container  wrap="nowrap" spacing={0} sx={{marginTop: 2, border: "1px solid black", marginLeft: 0, marginRight: 0,}}>
         <Grid>
           <Grid sx={{ width: 163, height: 108}}>
@@ -60,7 +62,7 @@ export default function ComprasActivas() {
           </Grid>
         </Grid>
         <Grid item xs={12} sm container>
-          <Grid container xs direction="column" spacing={2}>
+          <Grid item xs={12} sm container component="div" sx={{displey: "flex", alignContent: "center"}}>
             <Grid item xs >
               <Typography gutterBottom variant="subtitle1" component="div" >
               15va Compra Comunitaria
@@ -74,16 +76,19 @@ export default function ComprasActivas() {
       </Grid>
       <SigInButton fullWidth variant="contained" sx={{marginTop: 1}}>Comprar</SigInButton>
     
+    {/* Tarjeta proxima compras */}
+    <Grid container sx={{displey: "flex", justifyContent: {xs: "center", md: 'left'}}}>
       <Typography sx={{fontSize: 20, fontWeight: "bold", marginTop: 3}}>Próximas compras</Typography>
+      </Grid>
       <Divider component="div" />
-      <Grid container  wrap="nowrap" spacing={0} sx={{marginTop: 2, border: "1px solid black", marginLeft: 0, marginRight: 0,}}>
+      <Grid container wrap="nowrap" spacing={0} sx={{marginTop: 2, border: "1px solid black", marginLeft: 0, marginRight: 0,}}>
         <Grid>
           <Grid sx={{ width: 163, height: 108}}>
             <Img alt="complex" src="/src/img/compras.jpeg" sx={{m: 0 }}/>
           </Grid>
         </Grid>
         <Grid item xs={12} sm container component="div" sx={{displey: "flex", alignContent: "center"}}>
-          <Grid item xs direction="column" spacing={2}>
+          <Grid item wrap="nowrap" spacing={2}>
             <Grid item xs >
               <Typography gutterBottom variant="subtitle1" component="div" >
               15va Compra Comunitaria
@@ -92,14 +97,18 @@ export default function ComprasActivas() {
               Lorem ipsum dolor sit amet, consectetur.
               </Typography>
             </Grid>
-            <Grid item xs>
-            <Typography variant="subtitle1" component="div">
-            Comienza el 24/09/2021
-            </Typography>
-          </Grid>
           </Grid>
         </Grid>
+        <Grid item sx={{paddingTop: 2, paddingRight: 2,}}>
+            <Typography variant="subtitle1" component="div" sx={{textAlign: "end", display: {xs: "none", md: 'flex'}}} >
+            Comienza el 24/09/2021
+            </Typography>
+        </Grid>
       </Grid>
+      <Typography variant="subtitle1" component="div" sx={{textAlign: "end", display: {xs: 'block', sm: 'block', md: 'none'}}} >
+            Comienza el 24/09/2021
+      </Typography>
+      
     
     
     </Container>
