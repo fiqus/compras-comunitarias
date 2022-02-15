d26ff7d34565d9354c288b9dcb6ad0c3a691d266
ezequielleiter <ezequielleiter@gmail.com>
2022-02-15 12:56:42 -0300

    boton de agregar producto

--- frontend/frontendoffice/src/App.js
+++ frontend/frontendoffice/src/App.js
@@ -1,11 +1,13 @@
 import ComprasActivas from './pages/ComprasActivas';
+import { ListaProductos } from './pages/ListaProductos';
 import SignIn from './pages/SigIn';
 
 function App() {
   return (
     <>
   {/* <SignIn/> */}
-  <ComprasActivas/>
+  {/* <ComprasActivas/> */}
+  <ListaProductos/>
     </>
   );
 }
