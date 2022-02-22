22f2eb3d06da6f0c958f28b2fd60af6cc2d93d14
JeronimoC23 <jeronimoclinaz@gmail.com>
2022-02-22 11:45:11 -0300

    Formatting order views.py

--- compras/orders/views.py
+++ compras/orders/views.py
@@ -35,7 +35,8 @@ class OrderForm(ModelForm):
         model = Order
         fields = ['listing']
 
-#API
+#######   API   ########
+
 class ListingOrders(APIView):
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
@@ -44,7 +45,93 @@ class ListingOrders(APIView):
         listing = Listing.objects.get(pk=listing_id);
         return Response(listing.orders)
 
-#ACTUAL
+
+class get_listing_products(APIView):
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request, listing_id):
+        listing = Business().available_listings()
+        if (not listing):
+            return render(request, 'orders/no_listing.html')
+        listing = get_object_or_404(Listing, pk=listing_id, enabled=True)
+        
+        products = listing.listingproduct_set.all()
+        lp_str = serializers.serialize('json', products)
+        lp_json = json.loads(lp_str)
+        categories = {}
+        if listing:
+            for p in products:
+                category = str(p.product.category)
+                if category not in categories:
+                    categories[category] = []
+                
+                categories[category].append(p)
+        return Response(lp_json)
+
+
+class CreateOrder(APIView):
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def post(self, request, listing_id):
+        listing = Business().available_listings()
+        if (not listing):
+            return render(request, 'orders/no_listing.html')
+        listing = get_object_or_404(Listing, pk=listing_id, enabled=True)
+        order = Order.objects.filter(user=request.user, listing=listing).last()
+
+        OrderProductInlineFormset = inlineformset_factory(Order, OrderProduct, fields=('product', 'amount'),
+                                                      can_delete=False, extra=listing.products.count())
+        
+        if request.method == "POST":
+            form = OrderForm(request.POST, request.FILES, initial={'listing': listing}, instance=order)
+            formset = OrderProductInlineFormset(request.POST, request.FILES, instance=form.instance)
+            if form.is_valid() and formset.is_valid():
+
+                form.instance.user = request.user
+                form.instance.listing = listing
+                form.instance.orderproduct_set.all().delete()
+                form.save()
+                formset.save()
+                order = Order.objects.get(pk=form.instance.pk)
+                return HttpResponse(200)
+        
+        else:
+            form = OrderForm()
+            formset = OrderProductInlineFormset(instance=form.instance)
+        amounts = defaultdict(int)
+
+        if order:
+            for p in order.orderproduct_set.all():
+                amounts[p.product.id] = p.amount
+
+class get_listings(APIView):
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request):
+        listings = Listing.objects.filter(enabled=True)
+        listing_str = serializers.serialize('json', listings)
+        listing_json = json.loads(listing_str)
+        return Response(listing_json)
+
+
+class get_order(APIView):
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request, listing_id):
+        listing = get_object_or_404(Listing, pk=listing_id, enabled=True)
+        order = Order.objects.filter(user=request.user, listing=listing).last()
+        if order:
+            amounts = defaultdict(int)
+            for p in order.orderproduct_set.all():
+                amounts[p.product.id] = p.amount
+        order_str = serializers.serialize('json', [order])
+        order_json = json.loads(order_str)
+        return Response(order_json)
+
+####### END API #######
+
+####### ACTUAL #######
 def create_order(request, pk):
     
     listing = Business().available_listings()
@@ -93,102 +180,12 @@ def create_order(request, pk):
     return {listing}
 
 
-
-#API
-class get_listing_products(APIView):
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
-    def get(self, request, listing_id):
-        listing = Business().available_listings()
-        if (not listing):
-            return render(request, 'orders/no_listing.html')
-        listing = get_object_or_404(Listing, pk=listing_id, enabled=True)
-        
-        products = listing.listingproduct_set.all()
-        lp_str = serializers.serialize('json', products)
-        lp_json = json.loads(lp_str)
-        categories = {}
-        if listing:
-            for p in products:
-                category = str(p.product.category)
-                if category not in categories:
-                    categories[category] = []
-                
-                categories[category].append(p)
-        return Response(lp_json)
-
-
-#API 
-class CreateOrder(APIView):
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
-    
-    def post(self, request, listing_id):
-        listing = Business().available_listings()
-        if (not listing):
-            return render(request, 'orders/no_listing.html')
-        listing = get_object_or_404(Listing, pk=listing_id, enabled=True)
-        order = Order.objects.filter(user=request.user, listing=listing).last()
-
-        OrderProductInlineFormset = inlineformset_factory(Order, OrderProduct, fields=('product', 'amount'),
-                                                      can_delete=False, extra=listing.products.count())
-        
-        if request.method == "POST":
-            form = OrderForm(request.POST, request.FILES, initial={'listing': listing}, instance=order)
-            formset = OrderProductInlineFormset(request.POST, request.FILES, instance=form.instance)
-            if form.is_valid() and formset.is_valid():
-
-                form.instance.user = request.user
-                form.instance.listing = listing
-                form.instance.orderproduct_set.all().delete()
-                form.save()
-                formset.save()
-                order = Order.objects.get(pk=form.instance.pk)
-                return HttpResponse(200)
-        
-        else:
-            form = OrderForm()
-            formset = OrderProductInlineFormset(instance=form.instance)
-        amounts = defaultdict(int)
-
-        if order:
-            for p in order.orderproduct_set.all():
-                amounts[p.product.id] = p.amount
-
-
-
-# ACTUAL
 class Select_listing(LoginRequiredMixin, TemplateView):
     def get(self, request):
         listings = Listing.objects.filter(enabled=True)
         
         return render(request, 'orders/select_listing.html', {'listings':listings, 'object': request.user})
      
-# API
-class get_listings(APIView):
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
-    def get(self, request):
-        listings = Listing.objects.filter(enabled=True)
-        listing_str = serializers.serialize('json', listings)
-        listing_json = json.loads(listing_str)
-        return Response(listing_json)
-
-
-# API
-class get_order(APIView):
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
-    def get(self, request, listing_id):
-        listing = get_object_or_404(Listing, pk=listing_id, enabled=True)
-        order = Order.objects.filter(user=request.user, listing=listing).last()
-        if order:
-            amounts = defaultdict(int)
-            for p in order.orderproduct_set.all():
-                amounts[p.product.id] = p.amount
-        order_str = serializers.serialize('json', [order])
-        order_json = json.loads(order_str)
-        return Response(order_json)
 
 User = get_user_model()   
 class UserDetailView(LoginRequiredMixin, TemplateView):
